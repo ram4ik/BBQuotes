f103cce80235646e7b4cc10433a25e91edd67a74
ribragimov-mwb <ribragimov@malwarebytes.com>
2025-07-08 08:02:08 +0300

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Ramill Ibragimov on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
